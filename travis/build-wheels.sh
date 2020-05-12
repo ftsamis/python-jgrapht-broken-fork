9d07774c729fb17a8af210c097ba85b8e4b39ade
Fotis Tsamis <ftsamis@gmail.com>
2020-05-12 16:54:01 +0300

    Temporarily remove pip install -r requirements.txt from build-wheels.sh

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,7 +3,6 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/cp3*/bin; do
-    "${PYBIN}/pip" install -r /io/requirements.txt
     "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
 done
 

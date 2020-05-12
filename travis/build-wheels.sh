89687672e3ae5fe1a74e3d698158d72261dab9da
Fotis Tsamis <ftsamis@gmail.com>
2020-05-12 18:02:51 +0300

    Drop Python 3.5 temporarily.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,13 +4,13 @@ set -e -x
 cd /io
 
 # Compile wheels
-for PYBIN in /opt/python/cp3*/bin; do
+for PYBIN in /opt/python/cp3{6..8}*/bin; do
     "${PYBIN}/pip" install -r requirements/release.txt -r requirements/test.txt -r requirements/doc.txt -r requirements/default.txt
     "${PYBIN}/python" setup.py bdist_wheel
 done
 
 # Install packages and test
-for PYBIN in /opt/python/cp3*/bin; do
+for PYBIN in /opt/python/cp3{6..8}*/bin; do
     "${PYBIN}/pip" install python-jgrapht --no-index -f /io/dist
     (cd /io; "${PYBIN}/pytest")
 done

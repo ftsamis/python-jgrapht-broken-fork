fd7a8b11bff9542b3b27fcb4763573ce0c99affd
Fotis Tsamis <ftsamis@gmail.com>
2020-05-13 02:04:49 +0300

    Test mac

--- setup.py
+++ setup.py
@@ -27,11 +27,13 @@ elif sys.platform.startswith('darwin'):
     from distutils.sysconfig import get_config_vars
     conf_vars = get_config_vars()
     conf_vars['LDSHARED'] = conf_vars['LDSHARED'].replace('-bundle', '-dynamiclib')
+    # Temp
+    conf_vars['LDSHARED']+= ' -install_name @loader_path/' + capi_filename + ' '
     so_ext = '.dylib'
     capi_filename = 'libjgrapht_capi' + so_ext
     # On MacOS runtime_library_dirs doesn't work, and @loader_path is used instead of $ORIGIN
     #extra_link_args = ['-install_name', '@loader_path/' + capi_filename]
-    extra_link_args = ['-Wl,-rpath,@loader_path/' + capi_filename]
+    #extra_link_args = ['-Wl,-rpath,@loader_path/' + capi_filename]
 
 
 class BuildCapiCommand(Command):

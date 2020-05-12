bd29a4b1f4283cbd4a66689579675a0d615cc3b5
Fotis Tsamis <ftsamis@gmail.com>
2020-05-13 01:26:04 +0300

    Test mac

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ elif sys.platform.startswith('darwin'):
     capi_filename = 'libjgrapht_capi' + so_ext
     # On MacOS runtime_library_dirs doesn't work, and @loader_path is used instead of $ORIGIN
     #extra_link_args = ['-install_name', '@loader_path/' + capi_filename]
-    extra_link_args = ['-Wl,-rpath,' + capi_filename]
+    extra_link_args = ['-Wl,-rpath,@loader_path/' + capi_filename]
 
 
 class BuildCapiCommand(Command):
